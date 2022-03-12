e30408ed0d0874fdb28713a26110949d1615b21c
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-03-12 18:08:52 +1000

    Continuing to troubleshoot TypeError when passing data to the renderer

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
-    res.render("homepage", ...posts, {
+    res.render("homepage", posts, {
       logged_in: req.session.logged_in,
     });
   } catch (err) {
