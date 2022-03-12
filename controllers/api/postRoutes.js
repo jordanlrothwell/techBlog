b28e2160c915d701ab66aae4058548e610c626c0
Jordan Rothwell <jordanlrothwell@gmail.com>
2022-03-12 17:50:51 +1000

    Added seed data and updated views/controllers

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,15 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll();
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post("/", withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
@@ -29,7 +38,7 @@ router.delete("/:id", withAuth, async (req, res) => {
       return;
     }
 
-    res.status(200).json(PostData);
+    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
